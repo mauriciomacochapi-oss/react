379355068a23ec260c2868a5071acc14e80ecbb5
nain94607-lab <nain94607@gmail.com>
2026-09-02 16:36:26 -0300

    trabajo en proceso

--- clases-react/src/App.jsx
+++ clases-react/src/App.jsx
@@ -1,28 +1,35 @@
-//Para instalar dependencias (node_modules) hacer bun install 
-//Para correr el proyecto bun run dev 
+//Para instalar dependencias (node_modules) hacer bun install
+//Para correr el proyecto bun run dev
 
-import './App.css'
-import Persona from './componentes/Persona'
-import Perfil from './componentes/Perfil'
+import "./App.css";
+import Persona from "./componentes/Persona";
+import Perfil from "./componentes/Perfil";
+import TargetaPelicula from "./componentes/TargetaPelicula";
 
 //### Ejercicio 2.5 — Lista desde array
-
 //Tenés este array de películas:
 
-```jsx
-const peliculas = [ "Interestellar", "Batman"];
-```
+const peliculas = [
+  { id: 1, titulo: "Interstellar", año: 2014, vista: false },
+  { id: 2, titulo: "The Dark Knight", año: 2008, vista: true },
+  { id: 3, titulo: "Inception", año: 2010, vista: false },
+  { id: 4, titulo: "Oppenheimer", año: 2023, vista: true },
+];
 
 function App() {
-
   return (
     <>
-     <div>
-      <Persona/>
-      <Perfil nombre="Mauri" lenguajes={["JavaScript", "React", "CSS"]} /> 
-     </div>
+      <div>
+        <Persona />
+        <Perfil nombre="Mauri" lenguajes={["JavaScript", "React", "CSS"]} />
+      </div>
+      <div>
+        {peliculas.map((pelicula) => (
+          <TargetaPelicula titulo={pelicula.titulo} año={""} vista={""} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ clases-react/src/componentes/TargetaPelicula.jsx
@@ -0,0 +1,21 @@
+export default function TargetaPelicula(props){
+
+    const {titulo, año, vista} = props 
+    return(
+
+        <>
+        <h1>
+            {titulo}
+        </h1>
+        <><h1>
+            {año}
+        </h1></>
+        <h1>
+            {vista}
+        </h1>
+
+        </>
+
+    );
+
+}
